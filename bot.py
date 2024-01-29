d258a64b5688ee79a42eec757f8b66a84ee94817
vladimir-asus <vladimir@vladimir-E200HA>
2024-01-29 18:05:41 +0500

    Review fix

--- bot.py
+++ bot.py
@@ -9,21 +9,33 @@ from dotenv import load_dotenv
 
 logger = logging.getLogger(__name__)
 
-url = "https://dvmn.org/api/long_polling/"
-params = {"timestamp": time()}
-timeout = 120
+URL = "https://dvmn.org/api/long_polling/"
+PARAMS = {"timestamp": time()}
+TIMEOUT = 120
 
 
 def main():
+    load_dotenv()
+    devman_token = os.getenv("DEVMAN_TOKEN")
+    telegram_token = os.getenv("TELEGRAM_TOKEN")
+    telegram_chat_id = os.getenv("TELEGRAM_CHAT_ID")
+    headers = {"Authorization": f"Token {devman_token}"}
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+    handler = logging.StreamHandler(stream=sys.stdout)
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    bot = telegram.Bot(token=telegram_token)
+    logger.info(f"Инициализируем бота {bot}")
     while True:
         try:
             response = requests.get(
-                url, headers=headers, params=params, timeout=timeout)
+                URL, headers=headers, params=PARAMS, timeout=TIMEOUT)
             response.raise_for_status()
             check = response.json()
             status = check.get("status")
             if status == "timeout":
-                params["timestamp"] = check.get("timestamp_to_request")
+                PARAMS["timestamp"] = check.get("timestamp_to_request")
                 logger.debug("В ответе отсутствуют новые статусы")
             elif status == "found":
                 new_attempt = check["new_attempts"][0]
@@ -34,16 +46,13 @@ def main():
                     message += f'Есть замечания. {lesson_url}'
                 else:
                     message += 'Замечаний нет. Работа принята.'
-
-                logger.info(f"Инициализируем бота {bot}")
                 logger.info(f"Бот пытается отправить сообщение: {message}")
                 try:
-                    bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
+                    bot.send_message(chat_id=telegram_chat_id, text=message)
                 except telegram.error.TelegramError as error:
                     logger.error(error)
                 logger.debug("Бот отправил сообщение")
-
-            params["timestamp"] = check.get("last_attempt_timestamp")
+            PARAMS["timestamp"] = check.get("last_attempt_timestamp")
         except requests.exceptions.HTTPError as err:
             logger.error(f"Ошибка в процессе выполнения запроса: {err}")
             sleep(5)
@@ -58,15 +67,4 @@ def main():
 
 
 if __name__ == "__main__":
-    load_dotenv()
-    DEVMAN_TOKEN = os.getenv("DEVMAN_TOKEN")
-    TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
-    TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
-    bot = telegram.Bot(token=TELEGRAM_TOKEN)
-    headers = {"Authorization": f"Token {DEVMAN_TOKEN}"}
-    logger.setLevel(logging.DEBUG)
-    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
-    handler = logging.StreamHandler(stream=sys.stdout)
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
     main()
